53f1687ddb1492d24943ecec465067c13abf405c
Walterhart <walterwhart1@gmail.com>
2025-12-10 10:41:20 -0500

    Update header title to 'Final Hour'

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(props) {
   return (
     <header>
-      <h1>Hangman Game</h1>
+      <h1>Final Hour</h1>
       <p>Guess the word within {props.numberOfHearts} wrong guesses.</p>
     </header>
   );
